98b66fbc6163eefd39593d9ec52b49a78fd053a4
Heather Fryling <fryling.h@husky.neu.edu>
2020-05-18 10:13:27 -0700

    Solution 1

---
+++ ArrayDeletionProblems/src/main/java/removeelement/Solution1.java
@@ -0,0 +1,36 @@
+package removeelement;
+
+/**
+ * A method to remove a given value from an array of ints.
+ *
+ * @author hfryling
+ */
+
+public class Solution1 {
+  public int removeElement(int[] nums, int val) {
+    int length = nums.length;
+    for (int i = 0; i < length; i++) {
+      if (nums[i] == val) {
+        this.shiftLeft(nums, i, length);
+        length--;
+        i--;
+      }
+    }
+    return length;
+  }
+
+  private void shiftLeft(int[] nums, int pos, int length) {
+    for (int i = pos; i < length - 1; i++) {
+      nums[i] = nums[i + 1];
+    }
+  }
+
+  private void printArr(int[] nums, int length) {
+    System.out.println();
+    for (int i = 0; i < length; i++) {
+      System.out.print(nums[i]);
+    }
+    System.out.println();
+  }
+
+}
---
+++ ArrayDeletionProblems/src/main/java/removeelement/Solution2.java
@@ -0,0 +1,11 @@
+package removeelement;
+
+/**
+ * Optimized method to remove a given value from an array of ints.
+ *
+ * @author hfryling
+ */
+
+public class Solution2 {
+
+}
